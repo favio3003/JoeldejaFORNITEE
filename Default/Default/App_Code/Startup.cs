696ca9dfd7e9948ef92d62e789570b33b80b7f5a
Favio <favioregiscardona@gmail.com>
2018-05-07 16:42:01 -0400

    confirmaelProyecto

---
+++ Default/Default/App_Code/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Default.Startup))]
+namespace Default
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
